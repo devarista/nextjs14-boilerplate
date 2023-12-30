8e6a472c1343fbfce44620a305cf7fad7886b71c
Devarista <kampungvector@gmail.com>
2023-12-31 03:45:07 +0700

    refactor(setuping the tools (husky, commitlint, trpc, prisma, prettier)): setup tools
    setup husky, commitlint, trpc, prisma, prettier
    
    BREAKING CHANGE: none

---
+++ app/_trpc/Provider.tsx
@@ -0,0 +1,27 @@
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import { httpBatchLink } from "@trpc/client";
+import { ReactNode, useState } from "react";
+import { trpc } from "./client";
+
+const Provider = ({ children }: { children: ReactNode }) => {
+    const [queryClient] = useState(() => new QueryClient({}));
+    const [trpcClient] = useState(() =>
+        trpc.createClient({
+            links: [
+                httpBatchLink({
+                    url: "http://localhost:3000/api/trpc",
+                }),
+            ],
+        }),
+    );
+
+    return (
+        <trpc.Provider queryClient={queryClient} client={trpcClient}>
+            <QueryClientProvider client={queryClient}>
+                {children}
+            </QueryClientProvider>
+        </trpc.Provider>
+    );
+};
+
+export default Provider;
